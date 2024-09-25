9a90624c8988791dcaa51982c0374e290187df44
Sunstone-Summer-Project <ktech5047@gmail.com>
2024-09-25 17:50:28 +0530

    add .env file

--- sanity.cli.js
+++ sanity.cli.js
@@ -1,8 +1,12 @@
-import {defineCliConfig} from 'sanity/cli'
+import {defineCliConfig} from 'sanity/cli';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineCliConfig({
   api: {
     projectId: process.env.PROJECT_ID,
     dataset: process.env.DATASET
   }
-})
+});
